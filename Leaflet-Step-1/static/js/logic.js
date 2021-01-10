4827bc6c049af85fca10dc22d480c7941b5291c0
matthewbelevski <68624883+matthewbelevski@users.noreply.github.com>
2021-01-11 09:46:38 +1100

    markers plotted but not as circles

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,53 +1,78 @@
-// Creating map object
-var myMap = L.map("map", {
-    center: [61.0474, -152.3162],
-    zoom: 11
-  });
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+  console.log(data.features);
+});
+
+
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
   
-  // Adding tile layer to the map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    circle: circle
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Define streetmap and darkmap layers
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
+  });
+
+  // var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  //   maxZoom: 18,
+  //   id: "dark-v10",
+  //   accessToken: API_KEY
+  // });
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    //"Dark Map": darkmap
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
   }).addTo(myMap);
-  
-  // Store API query variables
-  var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-//   var date = "$where=created_date between'2016-01-01T00:00:00' and '2017-01-01T00:00:00'";
-//   var complaint = "&complaint_type=Rodent";
-//   var limit = "&$limit=10000";
-  
-  // Assemble API query URL
-  //var url = baseURL + date + complaint + limit;
-  
-  // Grab the data with d3
-  d3.json(baseURL, function(response) {
-
-    console.log(response);
-  
-    // Create a new marker cluster group
-    //var markers = L.markerClusterGroup();
-
-  
-      // Set the data location property to a variable
-
-  
-      for (var i = 0; i < response.length; i++) {
-        var location = response[i].geometry.location;
-        
-        L.circle([location.coordinates[1], location.coordinates[0]], {
-          fillOpacity: 0.75,
-          color: "white",
-          fillColor: "purple",
-          // Setting our circle's radius equal to the output of our markerSize function
-          // This will make our marker's size proportionate to its population
-          radius: markerSize(response[i].mag)
-        }).bindPopup("<h1>" + response[i].mag + "</h1> <hr> <h3>Population: " + response[i].place + "</h3>").addTo(myMap);
-    }}
-      
-
-  );
-  
+}
